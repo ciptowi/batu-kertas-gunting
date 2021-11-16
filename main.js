7ff7692fc235b327b34321ddda274ffbb8b9a262
ciptowi <ciptowidarto@gmail.com>
2021-11-16 20:32:56 +0700

    tambah fungsi pengkondisian game

--- main.js
+++ main.js
@@ -1,14 +1,42 @@
 // const key = ['p-batu','p-gunting','p-kertas'];
-const pBatu = document.querySelector('.pBatu');
-const pKertas = document.querySelector('.pKertas');
-const pGunting = document.querySelector('.pGunting');
+const pilBatu = document.querySelector('.batu');
+const pilKertas = document.querySelector('.kertas');
+const pilGunting = document.querySelector('.gunting');
+const result = document.querySelector('.result');
 
-pBatu.addEventListener('click', function(){
-  pBatu.style.backgroundColor = 'rgba(255,255,255,.5)';
+function game(player, com){
+  if(player === com){
+    // result.getElementsByClassName('result')[0].innrHTML = 'DRAW';
+    console.log(player +" vs "+ com +" : draw");
+  }
+  else if(player.length - com.length === -3 || player.length - com.length === 2 || player.length - com.length === 1){
+    // result.getElementsByClassName('result')[0].innrHTML = 'MENANG!';
+    console.log(player +" vs "+ com +" : "+ player + " menang");
+  }
+  else{
+    // result.getElementsByClassName('result')[0].innrHTML = 'KALAH!';
+    console.log(player +" vs "+ com +" : "+ player + " kalah");
+  }
+};
+
+function choiceCom(){
+  const nilai = Math.random();
+  if (nilai <= 0.33) return "batu";
+  if (nilai <= 0.67) return "kertas";
+  return "gunting";
+};
+
+pilBatu.addEventListener('click', function(){
+  const p = 'batu';
+  const c = choiceCom();
+  // let getResult = game(p, c)
+  pilBatu.style.backgroundColor = 'rgba(255,255,255,.5)';
+  console.log('player :'+ p);
+  console.log('computer :'+ c);
 });
-pKertas.addEventListener('click', function(){
-  pKertas.style.backgroundColor = 'rgba(255,255,255,.5)';
+pilKertas.addEventListener('click', function(){
+  pilKertas.style.backgroundColor = 'rgba(255,255,255,.5)';
 });
-pGunting.addEventListener('click', function(){
-  pGunting.style.backgroundColor = 'rgba(255,255,255,.5)';
+pilGunting.addEventListener('click', function(){
+  pilGunting.style.backgroundColor = 'rgba(255,255,255,.5)';
 });
